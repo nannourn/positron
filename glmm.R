ab475c3ab54ab5686795975cd4df9dfe6d77edcc
nannourn <nnourn@gmail.com>
2025-12-07 16:26:38 -0700

    glmm

---
+++ glmm.R
@@ -0,0 +1,15 @@
+# testing GLMMs
+# coded by: NOURN Nan
+# date: 2025-12-07
+
+# packages
+library(lme4)
+library(tidyverse)
+library(broom)
+library(here)
+
+# start with basic lm
+
+# upgraded to glm
+
+# now let's do a glmm
